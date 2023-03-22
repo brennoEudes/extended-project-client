e38dc2c806612f21949577c394de7a8e50b2147d
“Brenno <brennoeudes@gmail.com>
2023-03-22 13:55:29 -0300

    setup pág Home protegida

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,42 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import api from "../apis/api";
 import { Link } from "react-router-dom";
 
 function Home() {
+
+const [books, setBooks] = useState([]);
+
+useEffect (() => {
+  async function fetchBook() {
+    try {
+
+      const response = await api.get("/book");
+      setBooks(...response.data);
+
+    } catch (err) {
+      console.log (err);
+    }
+  }
+fetchBook();
+}, []);
+
+
+
   return (
     <div className="text-center">
-      <img
-        src="https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/84/original/logo-ironhack-blue.png"
-        alt="ironhack logo"
-      />
-      <h1>React IronPlate</h1>
-      <p>This is the homepage</p>
+      <h1>Library</h1>
+      <p>Read all books as posible! Enjoy!</p>
       <div className="d-flex flex-column align-items-center">
         <Link className="btn btn-lg btn-primary" to="/auth/signup">
-          Signup here!
+          Create book!
         </Link>
       </div>
+
+
+      {books.map ((currentBook) => {
+        return <h2>{currentBook.title}</h2>
+      })}
     </div>
   );
 }
