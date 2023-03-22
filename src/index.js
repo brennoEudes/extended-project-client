9cc7ffcbd8e1fd996c1dedb47391e6d80d8a3d37
“Brenno <brennoeudes@gmail.com>
2023-03-22 13:05:58 -0300

    instalação bootstrap, importação do bootstrap CSS no index.js e ajuste tag React Suspense

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Suspense>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.Suspense>,
   document.getElementById("root")
 );
