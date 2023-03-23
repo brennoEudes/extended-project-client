d259938a117b6960fcea00c5429c95aec15e8632
“Brenno <brennoeudes@gmail.com>
2023-03-23 19:40:42 -0300

    ajustes pág bookDetails

--- src/pages/BookDetails/index.js
+++ src/pages/BookDetails/index.js
@@ -1,71 +1,66 @@
 import { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import api from "../../apis/api";
-
+import BookForm from "../../components/BookForm/style.modules.css";
 import Button from "react-bootstrap/Button";
 
-function BookDetails () {
+function BookDetails() {
+  const params = useParams();
+  const navigate = useNavigate();
+  const [detail, setDetail] = useState({});
 
-    const params = useParams();
-    const navigate = useNavigate();
-    const [detail, setDetail] = useState({});
+  console.log(params);
 
-    console.log(params)
-  
-    useEffect(() => {
-      async function fetchDetail() {
-        try {
-          const response = await api.get(`/book/${params.bookId}`);
-          console.log(response);
-          setDetail(response.data);
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      fetchDetail();
-    }, []);
-  
-    async function handleDelete() {
+  useEffect(() => {
+    async function fetchDetail() {
       try {
-        const response = await api.delete(`/book/${params.bookId}`);
+        const response = await api.get(`/book/${params.bookId}`);
         console.log(response);
-        navigate("/");
+        setDetail(response.data);
       } catch (err) {
         console.log(err);
       }
     }
+    fetchDetail();
+  }, []);
 
-    return (
-        <>
-            <h1>Book Details</h1>
+  async function handleDelete() {
+    try {
+      const response = await api.delete(`/book/${params.bookId}`);
+      console.log(response);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-            <div className="container">
-        <div className="">
-        <img className="" src={detail.image} alt={detail.name} />
-          <h1 className="">{detail.name}</h1>
-          <div className="">
+  return (
+    <>
+      <div className="container">
+        <h1 className="bookFormHeader">Book Details</h1>
+        <div className="bookForm">
+          <div className="bookFormFields">
+            <div className="coverBook">
+              <img src={detail.coverImage} alt="coverImage" />
+            </div>
             <p>Title: {detail.title}</p>
             <p>Author: {detail.author}</p>
             <p>Synopsis: {detail.synopsis}</p>
             <p>Release Year: {detail.releaseYear}</p>
             <p>Genre: {detail.genre}</p>
-            <img src={detail.coverImage} alt="coverImage" />
-
-            <div className="">
-              <div className="">
-                <Link to={`/book-edit/${detail._id}`}>
-                  <Button variant="warning">Edit</Button>{" "}
-                </Link>
-                <Button onClick={handleDelete} variant="danger">
-                  Delete
-                </Button>{" "}
-              </div>
+            <div className="btn-editBook">
+              <Link to={`/book-edit/${detail._id}`}>
+                <Button variant="warning">Edit</Button>{" "}
+              </Link>
+              <Button onClick={handleDelete} variant="danger">
+                Delete
+              </Button>{" "}
             </div>
           </div>
         </div>
       </div>
-        </>
-    )
+    </>
+  );
 }
 
 export default BookDetails;
