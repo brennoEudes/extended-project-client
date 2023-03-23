ce2703a730d0c80a768d8d1431003b6e6f46cc9a
“Brenno <brennoeudes@gmail.com>
2023-03-23 00:31:35 -0300

    add / link production do deploy

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apis = {
   development: "http://localhost:1234",
-  production: "https://extended-project-library.cyclic.app/",
+  production: "https://extended-project-library.cyclic.app",
 };
 
 // Pré-configurando a URL padrão do nosso backend em uma instância do Axios
